a9968246cf8858e795a6301c829ffc3b666e15ec
MFREERKSEN <matthew_freerksen@cable.comcast.com>
2021-07-28 17:48:37 -0400

    before data layer part 2

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    public Employer() {}
 
+    @Valid
+    @NotBlank(message = "Required Field")
+    @Size(min=2,max=50,message = "Must be between 2 and 50 Char")
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,20 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    public Skill() {}
+
+    @Valid
+    @NotBlank
+    @Size
+    public String description;
 
 }
