f47b99e4a3ea9f1f9596d3542c94a675563ea11a
MFREERKSEN <matthew_freerksen@cable.comcast.com>
2021-07-29 14:30:40 -0400

    after 3 under controllers part 2

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/EmployerController.java
@@ -18,6 +18,14 @@ public class EmployerController {
     @Autowired
     private EmployerRepository employerRepository;
 
+    @RequestMapping("")
+    public String index(Model model) {
+
+        model.addAttribute("title", "Employers");
+
+        return "/employers";
+    }
+
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
         model.addAttribute(new Employer());
@@ -38,7 +46,7 @@ public class EmployerController {
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional<Employer> optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
